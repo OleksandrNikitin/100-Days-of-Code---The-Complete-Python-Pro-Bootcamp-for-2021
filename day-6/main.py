102c5641f2506fa3eef9b5963a3619d8caa4b181
Oleksandr Nikitin <oleksandr_nikitin@ukr.net>
2021-08-29 17:11:10 +0300

    Fix SonarLint code smell

--- day-6/main.py
+++ day-6/main.py
@@ -1,20 +1,25 @@
 def at_goal():
+    """This function has been declared outside of the current code"""
     pass
 
 
 def turn_left():
+    """This function has been declared outside of the current code"""
     pass
 
 
 def front_is_clear():
+    """This function has been declared outside of the current code"""
     pass
 
 
 def move():
+    """This function has been declared outside of the current code"""
     pass
 
 
 def right_is_clear():
+    """This function has been declared outside of the current code"""
     pass
 
 
