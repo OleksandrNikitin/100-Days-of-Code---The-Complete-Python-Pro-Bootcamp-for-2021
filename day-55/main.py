917d64f03568c972ef09aae0727afbe0d1a9d7c2
Oleksandr Nikitin <oleksandr_nikitin@ukr.net>
2021-09-28 19:40:50 +0300

    Add day-56

--- day-55/main.py
+++ day-55/main.py
@@ -1,34 +1,39 @@
-import time
+from flask import Flask
+import secrets
 
-current_time = time.time()
-print(current_time)
+random_number = secrets.choice(range(0, 9))
+print(random_number)
 
-
-def speed_calc_decorator(function):
-    def wrapper_function():
-        start_time = time.time()
-        function()
-        end_time = time.time()
-        print(f"{function.__name__} run speed: {end_time - start_time}s")
-
-    return wrapper_function
+app = Flask(__name__)
 
 
-@speed_calc_decorator
-def fast_function():
-    result = 0
-    for i in range(10000000):
-        result += i * i
-    return result
+@app.route("/")
+def home():
+    return (
+        "<h1>Guess a number between 0 and 9</h1>"
+        "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+    )
 
 
-@speed_calc_decorator
-def slow_function():
-    result = 0
-    for i in range(100000000):
-        result += i * i
-    return result
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > random_number:
+        return (
+            "<h1 style='color: purple'>Too high, try again!</h1>"
+            "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+        )
+
+    elif guess < random_number:
+        return (
+            "<h1 style='color: red'>Too low, try again!</h1>"
+            "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+        )
+    else:
+        return (
+            "<h1 style='color: green'>You found me!</h1>"
+            "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+        )
 
 
-fast_function()
-slow_function()
+if __name__ == "__main__":
+    app.run(debug=True)
